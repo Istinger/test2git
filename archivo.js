0df4d75bdac165c97bc29ae0da909431d25263e9
Jossuep <jossuepatric@hotmail.com>
2025-06-01 22:31:48 -0500

    Update JavaScript file

---
+++ archivo.js
@@ -0,0 +1,10 @@
+console.log("¡Hola, mundo! Este es un archivo JavaScript de ejemplo.");
+
+// Función para sumar dos números
+function sumar(a, b,c) {
+    return a + b+c;
+}
+
+// Ejemplo de uso
+const resultado = sumar(5, 8,3);
+console.log("El resultado de sumar 5 y 7 es:", resultado);
